b4673ca30a051d64c88ce72d76ecef5e1ed4fe10
HyeonJinJeon <nd3807@naver.com>
2023-12-07 16:12:59 +0900

    미로 탈출

--- DFS,BFS/5-2.py
+++ DFS,BFS/5-2.py
@@ -15,17 +15,24 @@ def bfs(x,y) :
 
     while queue :
         x, y = queue.popleft()
+
+        # 상하좌우를 확인하여 움직일 수 있는 곳인지 판별
         for i in range(4) :
             nx = x + dx[i]
             ny = y + dy[i]
 
+            # 주어진 영역 밖으로 이동하려고 할 때
             if nx<0 or ny<0 or nx>=n or ny>=m :
                 continue
+
+            # 벽으로 막혀있는 곳으로 이동하려고 할 때
             if graph[nx][ny] == 0 :
                 continue
+
+            # 이동할 수 있는 곳으로 이동 할 때
             if graph[nx][ny] == 1 :
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-    return graph[n-1][m-1]
+                graph[nx][ny] = graph[x][y] + 1 # 1인 곳을 이동하고 최단거리를 구하기 위해 전 영역의 수에 1을 더한다
+                queue.append((nx, ny)) # 이동한 곳을 큐에 넣어준다
+    return graph[n-1][m-1] #마지막 영역을 리턴
 
 print(bfs(0, 0))
---
+++ 정렬/선택정렬.py
@@ -0,0 +1,12 @@
+# 선택정렬 : 매번 가장 작은 것을 선택하여 맨 앞으로 차곡차곡 보내는 것
+
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(arr)) :
+    min_index = i #가장 작은 원소의 인덱스
+    for j in range(i+1, len(arr)) :
+        if arr[min_index] > arr[j] :
+            min_index = j
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+print(arr)
